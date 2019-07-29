691be9824f0edf18a643e138f38834d18e4e5b96
Marek Lotkowski <marek.lotkowski@gmail.com>
2019-07-29 21:28:16 +0200

    Update QueryType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -21,6 +21,15 @@ module Types
       argument :id, ID, required: true
     end
 
+    field :meta_data_by_context, [ContextMetaDataType], null: true do
+      description 'Query MetaData by contexts'
+      argument :contexts, [Types::ContextsEnum], required: false
+    end
+
+    def meta_data_by_context(contexts:)
+      contexts.any? ? Context.find(contexts) : Context.all
+    end
+
     def set(id:)
       begin
         collection = Collection.find(id)
