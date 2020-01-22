96985e4c905f6fb245410dce2b6a7b3388b32632
Max Albrecht <max.albrecht@zhdk.ch>
2020-01-22 16:33:37 +0100

    add root-page for development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+
+  def root
+    homepage = <<~HTML.html_safe
+    <!DOCTYPE html>
+    <h1>Madek GraphQL API</h1>
+    <h2>Development links:</h2>
+    <ul>
+      <li>
+        <a href="/graphql">GraphiQL API Endpoint (<samp>POST</samp> and <samp>GET</samp>)</a>
+        <ul>
+          <li><a href="/graphql?query={firstEntry:allMediaEntries(first:1){id}}"><samp>GET</samp> example</a></li>
+        </ul>
+      </li>
+      <li><a href="/graphql/graphiql">GraphiQL UI</a></li>
+    </ul>
+    HTML
+
+    render html: homepage
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get "/", to: "application#root"
+
   scope "/graphql" do
 
     # API endpoints
